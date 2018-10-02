46c6f2c6bc894bf112450949e71552f01ae9a151
vini23hot <vini23hot@gmail.com>
2018-10-02 14:32:40 -0300

    Added more effects to other elements

--- js/main.js
+++ js/main.js
@@ -2,3 +2,6 @@ $('#contributors figure').hover(e => {
   e.target.classList.toggle('blur')
   e.target.nextElementSibling.classList.toggle('visible')
 })
+$('#section-left, #section-right').hover(e => {
+  e.currentTarget.firstElementChild.classList.toggle('hover')
+})
