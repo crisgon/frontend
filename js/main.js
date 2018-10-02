4ee7b2056bc0a40d2fdbdf25418f09cf060c45fc
vini23hot <vini23hot@gmail.com>
2018-10-02 17:18:41 -0300

    Implemented test section and added jQuery Mask plugin

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
-$('#contributors figure').hover(e => {
-  e.target.classList.toggle('blur')
-  e.target.nextElementSibling.classList.toggle('visible')
+
+$('#contributors figure').hover(function() {
+  this.firstElementChild.classList.toggle('blur')
+  this.lastElementChild.classList.toggle('visible')
 })
-$('#section-left, #section-right').hover(e => {
-  e.currentTarget.firstElementChild.classList.toggle('hover')
+
+$('#section-left, #section-right').hover(function() {
+  this.firstElementChild.classList.toggle('hover')
+})
+
+$('#form-cep').submit(function(e)  {
+  e.preventDefault()
+  const responseBox = $('#response code')
+  const formattedInput = $(this).serialize().replace(/\D/g, '')
+  responseBox.text('Buscando...')
+
+  $.ajax({
+    url: `http://fastcep.appspot.com/v1/${formattedInput}`,
+    method: 'GET'
+  }).done(data => {
+    responseBox.text(JSON.stringify(data, null, 2))
+  }).fail(data => {
+    responseBox.text(`Ocorreu um erro ao buscar o CEP - ${data.status}`)
+  })
 })
